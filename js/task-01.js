5e654bf8ec8ca994123b92573da08917b3df0ee9
IrynaDiachenko <diachenko.iryna7@gmail.com>
2022-04-20 22:46:32 +0300

    Task 2

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const totalCategories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${totalCategories.length}`);
+
+totalCategories.forEach(item => {
+  const category = item.querySelector('h2');
+  const element = item.querySelector('ul');
+  const itemOfElement = element.querySelectorAll('li');
+  console.log(`Category: ${category.textContent}`);
+  console.log(`Elements: ${element.textContent}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const liElements = ingredients.map(el => {
+  const element = document.createElement('li');
+  element.textContent = el.charAt;
+  element.classList.add('item');
+  return element;
+});
+
+ingredients.append(...liElements);
