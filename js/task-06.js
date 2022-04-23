d9038c92ef7af80671896647d04128ae8a7ccb72
IrynaDiachenko <diachenko.iryna7@gmail.com>
2022-04-23 22:17:30 +0300

    Correct mark task 06

--- js/task-06.js
+++ js/task-06.js
@@ -4,10 +4,7 @@ inputEl.addEventListener('blur', onInputElBlur);
 
 function onInputElBlur(event) {
   inputEl.classList.add('invalid');
-  if (
-    event.currentTarget.value.length * 1 ===
-    inputEl.CDATA_SECTION_NODE.length * 1
-  ) {
+  if (event.currentTarget.value.length * 1 === inputEl.dataset.length * 1) {
     inputEl.classList.replace('invalid', 'valid');
   }
 }
